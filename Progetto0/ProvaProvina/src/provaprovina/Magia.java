db3495f4e2586fa290ee7cd069567630e5206697
Alaric112 <pataponale@gmail.com>
2022-10-11 19:00:41 +0200

    Create Magia.java

---
+++ Progetto0/ProvaProvina/src/provaprovina/Magia.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package provaprovina;
+
+/**
+ *
+ * @author patap
+ */
+public class Magia {
+    
+    
+    private int numero=0;
+
+    public Magia() {
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+   
+}
