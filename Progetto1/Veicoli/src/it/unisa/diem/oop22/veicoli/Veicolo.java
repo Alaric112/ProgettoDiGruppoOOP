378401e64931aad696e8a24cc9961277163a7655
Alaric112 <pataponale@gmail.com>
2022-10-12 09:04:03 +0200

    Completato abstract

--- Progetto1/Veicoli/src/it/unisa/diem/oop22/veicoli/Veicolo.java
+++ Progetto1/Veicoli/src/it/unisa/diem/oop22/veicoli/Veicolo.java
@@ -8,12 +8,47 @@ package it.unisa.diem.oop22.veicoli;
  *
  * @author patap
  */
-public abstract class  Veicolo{
+public abstract class Veicolo{
     
     private String numTelaio;
     private String modello;
     private String alimentazione;
     private String targa;
+
+    public Veicolo(String numTelaio, String modello, String alimentazione, String targa) {
+        this.numTelaio = numTelaio;
+        this.modello = modello;
+        this.alimentazione = alimentazione;
+        this.targa = targa;
     
+    
+    }
+
+    public String getNumTelaio() {
+        return numTelaio;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public String getAlimentazione() {
+        return alimentazione;
+    }
+
+    public String getTarga() {
+        return targa;
+    }
+
+    public void setTarga(String targa) {
+        this.targa = targa;
+    }
+    
+   public abstract boolean controllaTarga();
+    
+    @Override
+    public String toString() {
+        return "###" + "\nnumTelaio: " + numTelaio + "\nmodello: " + modello + "\nalimentazione: " + alimentazione+ "\ntarga: " + targa +'\n';
+    }
    
 }
