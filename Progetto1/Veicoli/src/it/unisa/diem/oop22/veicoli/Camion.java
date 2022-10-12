0655301bed9aca78def8781e5ca574ea9d216cc2
Alaric112 <pataponale@gmail.com>
2022-10-12 09:58:48 +0200

    efefef

--- Progetto1/Veicoli/src/it/unisa/diem/oop22/veicoli/Camion.java
+++ Progetto1/Veicoli/src/it/unisa/diem/oop22/veicoli/Camion.java
@@ -11,8 +11,9 @@ package it.unisa.diem.oop22.veicoli;
 public class Camion extends Veicolo {
    private int numeroAssi;
 
-    public Camion(String numTelaio, String modello, String alimentazione, String targa) {
+    public Camion(String numTelaio, String modello, String alimentazione, String targa, int numeroAssi) {
         super(numTelaio, modello, alimentazione, targa);
+        this.numeroAssi = numeroAssi;
     }
    
     public int getNumeroAssi() {
