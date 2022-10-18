c1896010546831c975ce3b2769f6d66571f297c7
Alaric112 <pataponale@gmail.com>
2022-10-18 19:05:15 +0200

    3

--- Progetto1/distributore/src/distributore/Distributore.java
+++ Progetto1/distributore/src/distributore/Distributore.java
@@ -4,6 +4,8 @@
  */
 package distributore;
 
+import java.util.Arrays;
+
 /**
  *
  * @author patap
@@ -34,18 +36,103 @@ public class Distributore {
         
     }
     
-    public void aggiungiBibita(Bibita tipo){
+    public boolean distributorePieno(){        
+        return riempBibite == maxBibite;
+    }
+    
+    public boolean memoriaTesseraEsaurita(){
+        return riempTessere == maxTessere;
+    }
+    
+    public void registraBevanda(Bibita tipo){
         
+        if(distributorePieno())
+            return;
         
+        bibite[riempBibite] = tipo;
+        riempBibite++;
+              
+    }
+    
+    public void caricaBevanda(String codice, int num){
+        
+        for(int i=0; i<riempBibite; i++){
+            
+            if(bibite[i].equals(codice)){
+                
+                bibite[i].setNumero(num);
+                return;
+            }
+            
+        }
+        System.out.println("Bibita non trovata");
         
     }
     
+    public void registraTessera(Tessera x){
+        if(memoriaTesseraEsaurita())
+            return;
+           
+        for(int i=0; i<riempTessere; i++){
+            
+            if(x.equals(tessere[i]))
+                return;
+            
+        }
+        
+        tessere[riempTessere] = x;
+        riempTessere++;
+        
+    }    
     
+    public void caricaSoldiTessera(int codice, double soldi){
+                
+        for(int i=0; i<riempTessere; i++){
+            
+            if(tessere[i].equals(codice)){
+                
+                tessere[i].aggiungiCredito(soldi);
+                return;
+            }
+         
+        }
+        
+        System.out.println("Tessera non trovata");
+    }
     
+    public double VisualizzaSaldoTessera(int codice){
+        
+            for(int i=0; i<riempTessere; i++){
+            
+            if(tessere[i].equals(codice)){
+                
+                
+                return tessere[i].getSaldo();
+            }
+         
+        }
+        
+        System.out.println("Tessera non trovata");
+        
+        return 0;
+    }
     
-    
-    
-    
+        public int VisualizzaBibiteDisponibili(String codice){
+        
+            for(int i=0; i<riempBibite; i++){
+            
+            if(bibite[i].equals(codice)){
+                
+                
+                return tessere[i].getNumero();
+            }
+         
+        }
+        
+        System.out.println("Il tipo della bibita non è stato trovato");
+        
+        return 0;
+    }
     
 }
 
