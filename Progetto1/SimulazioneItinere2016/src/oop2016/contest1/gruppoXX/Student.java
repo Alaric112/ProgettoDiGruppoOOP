9306266b5f431093097449335e87c15642aa0392
lorebor00 <lorebor00@gmail.com>
2022-10-28 11:29:01 +0200

    SimulazioneItinere2016

---
+++ Progetto1/SimulazioneItinere2016/src/oop2016/contest1/gruppoXX/Student.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop2016.contest1.gruppoXX;
+
+/**
+ *
+ * @author 39334
+ */
+public class Student {
+    
+}
