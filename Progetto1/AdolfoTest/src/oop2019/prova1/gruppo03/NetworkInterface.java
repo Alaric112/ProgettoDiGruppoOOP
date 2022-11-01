d14f5ae186ad6f3377e6ee88cbbf59190b944a70
Alessandro Accarino <pataponale@gmail.com>
2022-11-01 18:14:41 +0100

    Re-fixes

--- Progetto1/AdolfoTest/src/oop2019/prova1/gruppo03/NetworkInterface.java
+++
@@ -1,8 +0,0 @@
-package oop2019.prova1.gruppo03;
-
-public interface NetworkInterface {
-    
-    public void accept(NetworkInterface sourceInterface, int sourceAddress, int destAddress, String message);
-
-    public void connect(NetworkInterface other);
-}
