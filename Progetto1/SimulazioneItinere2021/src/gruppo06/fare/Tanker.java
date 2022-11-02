c8874bd5eb84917148996e10eaf9838dd7e0fc3e
mario <mario.berrino01@gmail.com>
2022-11-02 16:20:50 +0100

    Tanker

--- Progetto1/SimulazioneItinere2021/src/gruppo06/fare/Tanker.java
+++ Progetto1/SimulazioneItinere2021/src/gruppo06/fare/Tanker.java
@@ -8,6 +8,34 @@ package gruppo06.fare;
  *
  * @author patap
  */
-public class Tanker {
+public class Tanker extends truck{
+    private final int capacity;
+    private final Tanktype ttype;
+
+    public Tanker(TankType ttype, int capacity, String chassisID, DriveWheelType dw, int year, int month, int dayOfMonth, TruckType tt, int truckClass){
+        super(chassisID, dw, year,month, dayOfMonth,tt,truckClass);
+        this.ttype = ttype;
+        this.capacity = capacity;
+    }
+
+    public Tanker(TankType ttype, int capacity, String chassisID, DriveWheelType dw, int year, int month, int dayOfMonth, int truckClass) {
+        super(chassisID, dw, year,month, dayOfMonth,truckClass);
+        this.ttype = ttype;
+        this.capacity = capacity;      
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public Tanktype getTtype() {
+        return ttype;
+    }
+    
+    public boolean validateTruck(){
+       String regex = "^T[A-z]{3}[0-9]{3}$";
+       return this.;
+    }
+    
     
 }
