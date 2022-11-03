15fcd80da454921f22e79844a2d2a2dfb0bd92f1
mario <mario.berrino01@gmail.com>
2022-11-03 12:17:31 +0100

    a

--- Progetto1/SimulazioneLuci/src/oop2018/itinere1/gruppoXX/azioni/ImpostaStatoAccensione.java
+++ Progetto1/SimulazioneLuci/src/oop2018/itinere1/gruppoXX/azioni/ImpostaStatoAccensione.java
@@ -4,10 +4,39 @@
  */
 package oop2018.itinere1.gruppoXX.azioni;
 
+import oop2018.itinere1.gruppoXX.dispositivi.Attuatore;
+
 /**
  *
  * @author utente
  */
-public class ImpostaStatoAccensione {
+public class ImpostaStatoAccensione implements Azione{
+    private Attuatore attuatore;
+    private boolean SetOn;
+
+    public ImpostaStatoAccensione(Attuatore attuatore, boolean SetOn) {
+        this.attuatore = attuatore;
+        this.SetOn = SetOn;
+    }
     
+    @Override
+    public void esegui(){
+        if(SetOn){
+            attuatore.accendi();
+        }
+        else
+            attuatore.spegni();
+    }
+    
+    public String ToString(){
+        StringBuffer b = new StringBuffer("Tipo Azione: ");
+        if(SetOn){
+            b.append("accendi");
+        }
+        else{
+            b.append("spegni");
+        }
+        b.append(super.toString());
+        return b.toString();
+    }
 }
