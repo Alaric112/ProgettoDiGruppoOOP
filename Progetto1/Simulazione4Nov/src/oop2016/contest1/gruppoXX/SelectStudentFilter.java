cf3bde3d327e57aae45b235fe02c9f3b200ccddf
Crostino14 <91279322+Crostino14@users.noreply.github.com>
2022-11-03 16:48:17 +0100

    young

---
+++ Progetto1/Simulazione4Nov/src/oop2016/contest1/gruppoXX/SelectStudentFilter.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop2016.contest1.gruppoXX;
+
+/**
+ *
+ * @author PC
+ */
+public class SelectStudentFilter implements PersonFilter{
+
+    @Override
+    public boolean checkPerson(Person p) {
+            return p instanceof Student;
+    }
+}
