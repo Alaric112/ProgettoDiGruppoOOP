f2c39ec81d80223e18cb956d6a04adb76c52cc38
mario <mario.berrino01@gmail.com>
2022-11-03 12:15:07 +0100

    a

---
+++ Progetto1/SimulazioneLuci/src/oop2018/itinere1/gruppoXX/azioni/ImpostaLivello.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop2018.itinere1.gruppoXX.azioni;
+
+import oop2018.itinere1.gruppoXX.dispositivi.AttuatoreRegolabile;
+
+/**
+ *
+ * @author utente
+ */
+public class ImpostaLivello implements Azione {
+    private AttuatoreRegolabile ar;
+    private double livelloDaImpostare;
+
+    public ImpostaLivello(AttuatoreRegolabile ar, double LivelloDaImpostare) {
+        this.ar = ar;
+        this.LivelloDaImpostare = LivelloDaImpostare;
+    }
+    
+    public void Esegui() throws LivelloNonValidoException{
+        //fare try catch
+        ar.setLivello(livelloDaImpostare);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer b = new StringBuffer("Tipo azione: Accendi ");
+        b.append(valoreDiConfronto);
+        b.append(super.toString());
+        return b.toString();
+    }
+    
+    
+}
