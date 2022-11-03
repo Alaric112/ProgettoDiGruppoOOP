60bcac35df4a1b58976873d669782b65230790d1
lorebor00 <115097586+lorebor00@users.noreply.github.com>
2022-11-03 16:45:45 +0100

    a

---
+++ Progetto1/Simulazione4Nov/src/oop2016/contest1/gruppoXX/Student.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop2016.contest1.gruppoXX;
+
+/**
+ *
+ * @author darbo
+ */
+public class Student extends Person {
+    
+    private final String matricola;
+
+    public Student(String name, String surname, String matricola, int year,  int month, int dayOfMonth) {
+        
+        
+        super(name,surname,year,month,dayOfMonth);
+        this.matricola = matricola;
+        
+    }
+
+    @Override
+    public String toString() {
+        
+        StringBuffer b= new StringBuffer(super.toString());
+        b.append("\nmatricola=");
+        b.append(matricola);
+        
+        return b.toString();
+        
+    }
+    
+    
+    
+    
+
+}
