3b8c306d039d66c5f0da5c616a20508e2bff66b7
lorebor00 <115097586+lorebor00@users.noreply.github.com>
2022-11-09 13:47:33 +0100

    manifacturing

---
+++ Progetto1/ProvaIntercorso/src/group06/developed/ManifacturingDateComparator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package group06.developed;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author darbo
+ */
+public class ManifacturingDateComparator implements Comparator<Sensor>{
+    
+    @Override
+    public int compare(Sensor o1, Sensor o2){
+        
+       return o1.getManifacturingDate().compareTo(o2.getManifacturingDate());
+        
+       
+    }
+    
+}
