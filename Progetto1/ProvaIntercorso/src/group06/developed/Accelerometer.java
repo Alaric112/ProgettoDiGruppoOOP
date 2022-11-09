e2cc387c257e7a8863050e431dac3a3d276b6786
Alessandro Accarino <pataponale@gmail.com>
2022-11-09 14:02:24 +0100

    cefe

--- Progetto1/ProvaIntercorso/src/group06/developed/Accelerometer.java
+++ Progetto1/ProvaIntercorso/src/group06/developed/Accelerometer.java
@@ -4,6 +4,10 @@
  */
 package group06.developed;
 
+import group06.provided.Range;
+import group06.provided.SensorOutputType;
+import java.time.LocalDate;
+
 /**
  *
  * @author patap
@@ -13,5 +17,17 @@ public class Accelerometer extends Sensor {
     private final double range;
     private final int sensingAxis;
     private final double bandwith;
+
+    public Accelerometer(String partNumber, String manufacturer, double price, Range<Object> vs, SensorOutputType sot, LocalDate manifacturingDate, double range, int sensingAxis, double bandwith) {
+        super(partNumber, manufacturer, price, vs, sot, manifacturingDate);
+        this.range = range;
+        this.sensingAxis = sensingAxis;
+        this.bandwith = bandwith;
+    }
+    
+        public Accelerometer(String partNumber, String manufacturer, double price, Range<Object> vs, SensorOutputType sot, LocalDate manifacturingDate, double range, double bandwith) {
+        this(partNumber, manufacturer, price, vs, sot, manifacturingDate, double range, 3, double bandwidth);
+        
+    }
     
 }
