a587af3de2bd4c4129f681b26d81dab49bad4112
Alessandro Accarino <pataponale@gmail.com>
2022-11-09 14:00:06 +0100

    3fde

---
+++ Progetto1/ProvaIntercorso/src/group06/developed/Accelerometer.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package group06.developed;
+
+/**
+ *
+ * @author patap
+ */
+public class Accelerometer extends Sensor {
+    
+    private final double range;
+    private final int sensingAxis;
+    private final double bandwith;
+    
+}
