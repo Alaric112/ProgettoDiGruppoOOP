34772e50166c4a56ddace795866bdf2c217fdbec
lorebor00 <115097586+lorebor00@users.noreply.github.com>
2022-12-20 10:05:38 +0100

    asd

---
+++ Progetto1/gruppo06/src/gruppo06/TFQuestion.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gruppo06;
+
+/**
+ *
+ * @author darbo
+ */
+public class TFQuestion extends Question{
+    
+    private boolean answer;  //accesso privato e prevede il getter, BOOLEAN PERCHE' puo essere unicamente vero o falso
+
+    public TFQuestion(String questionTest, boolean answer) {
+        
+        super(questionTest);
+        this.answer = answer;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+    
+    
+    
+}
