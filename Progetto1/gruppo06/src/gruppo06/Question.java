90a0664188515bdce4c1d049c0b3f4162fbea901
lorebor00 <115097586+lorebor00@users.noreply.github.com>
2022-12-20 09:54:36 +0100

    sda

---
+++ Progetto1/gruppo06/src/gruppo06/Question.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gruppo06;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Gruppo 06
+ */
+public abstract class Question implements Serializable {
+    
+    private String questionTest;
+    private int id=0;
+
+    /**
+     * Costruttore della classe astratta Question
+     * @param questionTest 
+     */
+    public Question(String questionTest) {
+        this.questionTest = questionTest;
+        id++;
+    }
+
+    public String getQuestionTest() {
+        return questionTest;
+    }
+
+    public void setQuestionTest(String questionTest) {
+        this.questionTest = questionTest;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Question other = (Question) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
